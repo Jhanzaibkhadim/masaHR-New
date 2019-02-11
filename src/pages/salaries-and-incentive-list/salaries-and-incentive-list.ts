2b781ba9ff46fe1e9eb64e46ce93eaf4cfea7fd8
Jhanzeb Khadim <dsu.cs.1.13.012@gmail.com>
2019-02-11 22:25:48 +0500

    3333

--- src/pages/salaries-and-incentive-list/salaries-and-incentive-list.ts
+++ src/pages/salaries-and-incentive-list/salaries-and-incentive-list.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SalariesAndIncentiveListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+ 
 @Component({
   selector: 'page-salaries-and-incentive-list',
   templateUrl: 'salaries-and-incentive-list.html',
