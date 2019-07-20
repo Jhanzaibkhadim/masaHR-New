86cd8c8bbe09d854bf590d332bde71ba6985faff
salmanullahkhan <salmanullahkhan86@gmail.com>
2019-07-20 12:14:24 +0500

    12344321234

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -64,9 +64,9 @@ export class Constants {
     static GET_EMP_DETAIL = "employee_job?employee_id="
 
 
-    static INSERT_ATTENDECE = "insert_attendance/"
+    static INSERT_ATTENDECE = "insert_attendance"
 
-    static TODAY_ATTENDANCE = 'attendance_today/'
+    static TODAY_ATTENDANCE = 'attendance_today'
 
     static ATTENDANCE_STATUS = 'attendance_status?employee_id='
 
